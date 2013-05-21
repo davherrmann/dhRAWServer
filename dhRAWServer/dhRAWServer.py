dd722663b9174896cf6eec557e6b667514803a5a
davherrmann <davherrmann@googlemail.com>
2013-05-21 20:22:28 +0100

    changed from JSON to XML

--- dhRAWServer/dhRAWServer.py
+++ dhRAWServer/dhRAWServer.py
@@ -4,16 +4,16 @@ Created on 21 May 2013
 @author: dherrmann
 '''
 
-import json
+import util_xml
 import cherrypy
 from util_exif import extract_exifs_from_folder as extract_exifs
 
 imageSourceFolder = "./imageSource"
 
-class ImageInfoJSON(object):
+class ImageInfoXML(object):
     @cherrypy.expose
     def index(self):
-        return json.dumps(extract_exifs(imageSourceFolder)).replace(", ", ",<br>")
+        return util_xml.to_xml(extract_exifs(imageSourceFolder))
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0'} )   
-cherrypy.quickstart(ImageInfoJSON())
+cherrypy.quickstart(ImageInfoXML())
--- dhRAWServer/util_exif.py
+++ dhRAWServer/util_exif.py
@@ -26,7 +26,7 @@ def extract_exif_from_jpg(path, tags=EXIF_TAGS_SHOWN):
         #open image in binary reading mode
         with open(path, 'rb') as image:
             tags_all = exif.process_file(image)
-            tags_shown = {tag: str(tags_all[tag]) for tag in tags}
+            tags_shown = {tag.split(" ")[-1:][0]: str(tags_all[tag]) for tag in tags}
         return tags_shown
     except IOError:
         return {}
@@ -36,4 +36,5 @@ def extract_exifs_from_folder(path, tags=EXIF_TAGS_SHOWN):
     exifs = {}
     for image in glob.glob(os.path.join(path, "*.JPG")):
         exifs[image] = extract_exif_from_jpg(image, tags)
+        exifs[image]["path"] = image
     return exifs
